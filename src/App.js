ee861bd9cd3237730cfb2635ed48830ae2dd5bea
chystarr <chynnastarr2@gmail.com>
2023-05-04 01:37:00 -0400

    Added links to account and cart pages to nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout.js';
+import Catalog from './components/Catalog.js';
+import Account from './components/Account.js';
+import Cart from './components/Cart.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Layout />}></Route>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Catalog />} />
+          <Route path='account' element={<Account />} />
+          <Route path='cart' element={<Cart />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,21 +2,35 @@ import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link, Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
-    <AppBar position='static'>
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          <Typography variant='h6'>
-            Website Name
-          </Typography>
-          <p>Search</p>
-          <p>Account</p>
-          <p>Cart</p>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <div>
+      <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <Typography variant='h6' component={Link} to='/'>
+              Website Name
+            </Typography>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <IconButton component={Link} to='account'>
+              <AccountCircleIcon />
+            </IconButton>
+            <IconButton component={Link} to='cart'>
+              <ShoppingCartIcon />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Outlet />
+    </div>
   );
 }
 
