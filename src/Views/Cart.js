1825af281837478ddfaa2bd84950fab06baf4704
TienNguyen93 <nguyentien9933@gmail.com>
2023-05-10 21:34:13 -0400

    resolve merge conflicts

--- src/Views/Cart.js
+++ src/Views/Cart.js
@@ -1,23 +1,37 @@
-import Cards from "../components/Cards"
+import Cards from "../components/Cards";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
+import { useState, useEffect } from "react";
+import { getProductByType } from "../api/index";
 
 const Cart = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    getProductByType("lipstick").then((response) => {
+      setProducts(response);
+    });
+  }, [])
+
+  console.log("products here", products)
+
   return (
     <Container>
-      <Typography variant="h3" sx={{mt: 2}}>
-        Shopping cart page
+      <Typography variant="h3" sx={{ mt: 2 }}>
+        Shopping cart
       </Typography>
-      <Box sx={{
-        display: "flex",
-        alignItems: 'flex-start',
-        flexDirection: 'column'
-      }}>
-        <p>henlo1</p>
-        <p>djdjgksjdfkjs;</p>
-        {/* <Cards /> */}
-      </Box>
+
+      {products.map(product => (
+        <Grid item key={product.id} md={2.4}>
+          <Card>
+            
+          </Card>
+        </Grid>
+      ))}
     </Container>
   );
 }
