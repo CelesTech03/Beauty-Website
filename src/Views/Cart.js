27839ecf0c88eb529ec6f52a704ac42ad267797e
TienNguyen93 <nguyentien9933@gmail.com>
2023-05-17 15:21:36 -0400

    basic layout for Checkout page & refactor Order  Summary component

--- src/Views/Cart.js
+++ src/Views/Cart.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
 import {
   Card,
   CardMedia,
@@ -17,12 +16,11 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
 import { getProductByType } from "../api/index";
+import Summary from "../utils/Summary";
 
 const Cart = () => {
   const [products, setProducts] = useState([])
   const [count, setCount] = useState(0)
-  const titles = ['Merchandise subtotal', 'Shipping & Handling', 'Tax']
-  const prices = ['$35.75', 'TBD', 'TBD']
 
   useEffect(() => {
     getProductByType("foundation").then((response) => {
@@ -125,76 +123,7 @@ const Cart = () => {
               price={product.price} />
           ))}
         </Card>
-
-        <Box sx={{ display: 'flex', flexDirection: "column" }}>
-          <Card>
-            <CardContent>
-              <Typography
-                align="left"
-                variant="h6"
-                sx={{ fontWeight: 'bold' }}
-                gutterBottom>
-                Order Summary
-              </Typography>
-
-              <Box sx={{ display: 'flex' }}>
-                <Box sx={{ mr: 8 }}>
-                  {titles.map((title, index) => (
-                    <Typography
-                      key={index}
-                      align='left'
-                      variant="subtitle2"
-                      gutterBottom>
-                      {title}
-                    </Typography>
-                  ))}
-                </Box>
-
-                <Box>
-                  {prices.map((price, index) => (
-                    <Typography
-                      key={index}
-                      align='right'
-                      variant="subtitle2"
-                      sx={{ fontWeight: 'bold' }}
-                      gutterBottom>
-                      {price}
-                    </Typography>
-                  ))}
-                </Box>
-              </Box>
-              <Divider variant="middle" sx={{ mt: 1 }} />
-              <Box sx={{ mt: 1, display: 'flex', justifyContent: 'space-between' }}>
-                <Typography
-                  align="left"
-                  variant="h6"
-                  sx={{ fontWeight: 'bold' }}
-                  gutterBottom>
-                  Estimated Total
-                </Typography>
-                <Typography
-                  align="left"
-                  variant="subtitle1"
-                  sx={{ fontWeight: 'bold' }}
-                  gutterBottom>
-                  $35.75
-                </Typography>
-              </Box>
-              <Divider variant="middle" sx={{ mt: 1 }} />
-              <Button
-                variant="contained"
-                sx={{ mt: 3, width: '100%', backgroundColor: "#ff4500" }}
-                component={Link}
-                to="/checkout"
-              >
-                <ShoppingCartCheckoutIcon />
-                <Typography variant="subitle1" sx={{ fontWeight: 'bold', ml: 1 }}>
-                  Checkout
-                </Typography>
-              </Button>
-            </CardContent>
-          </Card>
-        </Box>
+        <Summary title="Checkout" icon={<ShoppingCartCheckoutIcon />} />
       </Box>
     </Container>
   );
--- src/Views/Checkout.js
+++ src/Views/Checkout.js
@@ -1,82 +1,145 @@
+import { useState } from "react";
 import {
   Card,
-  CardMedia,
   Typography,
   Box,
   Container,
-  Grid,
   CardContent,
   Button,
   Divider,
-  TextField
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel
 } from "@mui/material";
 
+import Summary from "../utils/Summary";
+
 const Checkout = () => {
+  const [value, setValue] = useState('same')
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  // reusable form for shipping & billing address
+  const AddressForm = ({ name }) => {
+    return (
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 0.5 },
+        }}>
+        <Card>
+          <Typography variant="h6" align='left' sx={{ ml: 2.5, mt: 2, mb: -3 }}>
+            {name}
+          </Typography>
+          <CardContent>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <TextField label="Country/Region" variant="standard" sx={{ width: '100%' }} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <TextField label="First name" variant="standard" sx={{ width: '100%' }} />
+              <TextField label="Last name" variant="standard" sx={{ width: '100%' }} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <TextField label="Address" variant="standard" sx={{ width: '100%' }} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <TextField label="Apartment, suite, etc. (optional)" variant="standard" sx={{ width: '100%' }} />
+            </Box>
+            <Box>
+              <TextField label="City" variant="standard" />
+              <TextField label="State" variant="standard" />
+              <TextField label="ZIP code" variant="standard" />
+            </Box>
+            <Box>
+              <TextField label="Phone" variant="standard" sx={{ width: '100%' }} />
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    )
+
+  }
+
   return (
-    <Container sx={{mb: 5}}>
+    <Container sx={{ mb: 5 }}>
       <Typography variant="h4" align="left" sx={{ mt: 2 }} gutterBottom >
         Checkout
       </Typography>
 
       <Divider sx={{ mb: 1, background: '#838383', borderBottomWidth: 1.5 }} />
 
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box 
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1},
-          }}>
-          <Card sx={{mt: 1}}>
-            <Typography variant="h6" align='left' sx={{ ml: 2.5, mt: 2, mb: -3 }}>
-              Shipping Address
-            </Typography>
-            <CardContent>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-                <TextField label="Country/Region" variant="standard" sx={{width: '100%'}} />
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-start'}}>
-                <TextField label="First name" variant="standard" />
-                <TextField label="Last name" variant="standard" />
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-start'}}>
-                <TextField label="Address" variant="standard" sx={{width: '100%'}} />
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-                <TextField label="Apartment, suite, etc. (optional)" variant="standard" sx={{width: '100%'}}/>
-              </Box>
-              <Box>
-                <TextField label="City" variant="standard" />
-                <TextField label="State" variant="standard" />
-                <TextField label="ZIP code" variant="standard" />
-              </Box>
-              <Box>
-                <TextField label="Phone" variant="standard" sx={{width: '100%'}}/>
-              </Box>
-            </CardContent>
-          </Card>
-        </Box>
-
-        <Box sx={{backgroundColor: 'pink'}}>
-          put order summary here, like in cart page
-        </Box>
-
-        {/* <Box sx={{ display: 'flex', justifyContent: 'flex-start', flexDirection: 'column' }}>
-          <Box>
-            <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
-              Billing Address
-            </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+        <Box>
+          <Box sx={{ mb: 4 }}>
+            <AddressForm name="Shipping Address" />
           </Box>
-          <Button
-            variant="contained"
-            sx={{ mt: 3, width: '100%', backgroundColor: "#ff4500" }}
-          >
-            <Typography variant="subitle1" sx={{ fontWeight: 'bold', ml: 1 }}>
-              Place Order
-            </Typography>
-          </Button>
-        </Box> */}
-      </Box>
 
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 0.5 },
+            }}>
+            <Card sx={{ mt: 1 }}>
+              <Typography variant="h6" align='left' sx={{ ml: 2.5, mt: 2, mb: -3 }}>
+                Payment
+              </Typography>
+              <CardContent>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                  <TextField label="Card number" variant="standard" sx={{ width: '100%' }} />
+                </Box>
+
+                <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                  <TextField label="Name on card" variant="standard" sx={{ width: '100%' }} />
+                </Box>
+
+                <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                  <TextField label="Expiration date (MM / YY)" variant="standard" sx={{ width: '100%' }} />
+                  <TextField label="Security code" variant="standard" sx={{ width: '100%' }} />
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+
+          <Box>
+            <CardContent
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                flexDirection: 'column'
+              }}>
+              <Typography variant="h6" >
+                Billing Address
+              </Typography>
+              <Typography variant="subtitle1" >
+                Select the address that matches your card or payment method.
+              </Typography>
+
+              <FormControl sx={{ mt: 1, mb: 1 }}>
+                <RadioGroup
+                  aria-labelledby="demo-controlled-radio-buttons-group"
+                  name="controlled-radio-buttons-group"
+                  value={value}
+                  onChange={handleChange}
+                  sx={{ display: 'flex', flexDirection: 'row' }}
+                >
+                  <FormControlLabel value="same" control={<Radio />} label="Same as shipping address" />
+                  <FormControlLabel value="different" control={<Radio />} label="	Use a different billing address" />
+                </RadioGroup>
+              </FormControl>
+              {value === 'different' ? <AddressForm /> : ""}
+            </CardContent>
+          </Box>
+        </Box>
+        <Box sx={{width: '40%'}}>
+          <Summary title="Pay now" />
+        </Box>
+      </Box>
     </Container>
   )
 }
