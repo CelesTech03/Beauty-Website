ff833aa357cd29974c6d30c0b4c5b9cdfa2cf255
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-05-24 22:17:50 -0400

    https

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const getProductByType = (type) => {
   return Axios.get(
-    `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`
+    `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`
   )
     .then((response) => {
       return response.data.slice(32, 37); // Only display 5 cards (with valid image links)
@@ -14,7 +14,7 @@ export const getProductByType = (type) => {
 };
 
 export const getProductById = (id) => {
-  return Axios.get(`http://makeup-api.herokuapp.com/api/v1/products/${id}.json`)
+  return Axios.get(`https://makeup-api.herokuapp.com/api/v1/products/${id}.json`)
     .then((response) => {
       return response.data;
     })
